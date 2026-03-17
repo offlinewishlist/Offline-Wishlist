146e559abd51f485295187010d5d7d4f95be85d4
Lakshmi Kalyani Pasumarthi <offlinewishlist@gmail.com>
2026-03-17 12:00:00 +0530

    Home Screen UI state management done

--- app/src/main/java/com/example/calmlist/presentation/ViewModel/WishDetailViewModel.kt
+++ app/src/main/java/com/example/calmlist/presentation/ViewModel/WishDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.calmlist.presentation.ViewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.calmlist.data.Repo.WishRepository
@@ -10,24 +11,56 @@ import kotlinx.coroutines.launch
 
 
 class WishDetailViewModel(
-    private val repository: WishRepository
+    private val repository: WishRepository,
 ) : ViewModel() {
+    private val _HomeScreenstate = mutableStateOf(HomeScreenSate())
+    val logINScreenstate = _HomeScreenstate
 
-    val state = MutableStateFlow<ResultState<Unit>>(ResultState.Loading)
+    //  val state = MutableStateFlow<ResultState<Unit>>(ResultState.Loading)
 
     fun editWish(wish: Wish) {
         viewModelScope.launch {
-            repository.editWish(wish).collect {
-                state.value = it
+            repository.editWish(wish).collect { result ->
+                when (result) {
+                    ResultState.Loading -> HomeScreenSate(isLoading = true)
+                    is ResultState.Succes -> {
+                        _HomeScreenstate.value = HomeScreenSate(
+                            success = true,
+                        )
+                    }
+
+                    is ResultState.error -> {
+                        _HomeScreenstate.value = HomeScreenSate(error = result.message)
+                    }
+                }
             }
         }
     }
 
     fun deleteWish(wishId: String) {
         viewModelScope.launch {
-            repository.deleteWish(wishId).collect {
-                state.value = it
+            repository.deleteWish(wishId).collect { result ->
+                when (result) {
+                    ResultState.Loading -> HomeScreenSate(isLoading = true)
+                    is ResultState.Succes -> {
+                        _HomeScreenstate.value = HomeScreenSate(
+                            success = true,
+                        )
+
+                    }
+
+                    is ResultState.error -> {
+                        _HomeScreenstate.value = HomeScreenSate(error = result.message)
+                    }
+                }
             }
         }
     }
 }
+
+data class HomeScreenSate(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+
+    val success: Boolean? = false,
+)
